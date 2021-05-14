97ff8963720de3248905b53b95237d4ad1e74d66
Ayush Garg <ayush.garg@tripock.com>
2021-05-14 17:26:37 +0530

    priority queue using max heap done....

--- princeton-part-1/priority_queues/pq.c
+++ princeton-part-1/priority_queues/pq.c
@@ -13,3 +13,57 @@ void printArray(int A[], int size){
   }
   printf("\n");
 }
+
+
+
+void printHeap(int heap[], int size){
+  for(int i=1; i<=size; i++){
+    printf("%d ", heap[i]);
+  }
+  printf("\n");
+}
+
+
+int isLess(int a, int b){
+  return a < b;
+}
+
+
+void swim(int heap[], int position){
+  int k = position;
+  while(k/2 >= 1){
+    if(isLess(heap[k/2], heap[k])){
+      exchange(&heap[k/2], &heap[k]);
+      k = k/2;
+    }
+    else{
+      break;
+    }
+  }
+}
+
+
+void sink(int heap[], int position){
+  int present_elem = position;
+  int k = 1;
+  while(2*k < present_elem){
+    if(isLess(heap[2*k], heap[2*k + 1])){
+      if(isLess(heap[k], heap[2*k + 1])){
+	exchange(&heap[k], &heap[2*k + 1]);
+	k = 2*k + 1;
+      }
+      else{
+	break;
+      }
+    }
+    else{
+      if(isLess(heap[k], heap[2*k])){
+	exchange(&heap[k], &heap[2*k]);
+	k = 2*k;
+      }
+      else{
+	break;
+      }
+    }
+  }
+}
--- princeton-part-1/priority_queues/pq.h
+++ princeton-part-1/priority_queues/pq.h
@@ -1,3 +1,9 @@
 void exchange(int *a, int *b);
 
 void printArray(int A[], int size);
+
+void printHeap(int A[], int size);
+
+void swim(int heap[], int position);
+
+void sink(int heap[], int position);
