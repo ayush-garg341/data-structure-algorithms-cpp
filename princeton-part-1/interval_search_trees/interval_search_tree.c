b1ded0e511e21b5dbc92efd066cc9a0e36488352
Ayush Garg <ayush.garg@tripock.com>
2021-05-23 06:28:36 +0530

    more modularity added by defineing struct interval

--- princeton-part-1/interval_search_trees/interval_search_tree.c
+++ princeton-part-1/interval_search_trees/interval_search_tree.c
@@ -15,23 +15,23 @@ void updateMaxValForNode(struct ist_node *current_node, struct ist_node *latest_
 
 
 
-struct ist_node * insertInterval(int low, int high, struct ist_node *root){
+struct ist_node * insertInterval(struct interval interval, struct ist_node *root){
   if(root==NULL){
     struct ist_node* root = (struct ist_node*)malloc(sizeof(struct ist_node));
-    root->left_val = low;
-    root->right_val = high;
-    root->max_so_far = high;
+    (root->interval).left_val = interval.left_val;
+    (root->interval).right_val = interval.right_val;
+    root->max_so_far = interval.right_val;
     root->left_tree = NULL;
     root->right_tree = NULL;
     latest_node = root;
     return root;
   }
   else{
-    if(low < root->left_val){
-      root->left_tree = insertInterval(low, high, root->left_tree);
+    if(interval.left_val < (root->interval).left_val){
+      root->left_tree = insertInterval(interval, root->left_tree);
     }
-    else if(low > root->left_val){
-      root->right_tree = insertInterval(low, high, root->right_tree);
+    else if(interval.left_val > (root->interval).left_val){
+      root->right_tree = insertInterval(interval, root->right_tree);
     }
 
     updateMaxValForNode(root, latest_node);
@@ -41,17 +41,17 @@ struct ist_node * insertInterval(int low, int high, struct ist_node *root){
 }
 
 
-void searchIntersectingInterval(struct ist_node *root, int low, int high){
+void searchIntersectingInterval(struct ist_node *root, struct interval interval){
   struct ist_node *x = root;
   while(x!=NULL){
-    if(intersects(x, low, high)){
-      printf("new interval (%d, %d) intersects with  existing interval (%d, %d) \n", low, high, x->left_val, x->right_val);
+    if(intersects(x, interval)){
+      printf("new interval (%d, %d) intersects with  existing interval (%d, %d) \n", interval.left_val, interval.right_val, (x->interval).left_val, (x->interval).right_val);
       return;
     }
     else if(x->left_tree==NULL){
       x = x->right_tree;
     }
-    else if(x->left_tree->max_so_far < low){
+    else if(x->left_tree->max_so_far < interval.left_val){
       x = x->right_tree;
     }
     else{
@@ -63,9 +63,8 @@ void searchIntersectingInterval(struct ist_node *root, int low, int high){
 }
 
 
-int intersects(struct ist_node *x, int low, int high){
-  printf("%d %d %d %d\n", x->right_val, x->left_val, low, high);
-  if(x->right_val < low || x->left_val > high){
+int intersects(struct ist_node *x, struct interval interval){
+  if((x->interval).right_val < interval.left_val || (x->interval).left_val > interval.right_val){
     return 0;
   }
   return 1;
@@ -76,22 +75,49 @@ void in_order_traversal(struct ist_node *root){
     return;
   }
   in_order_traversal(root->left_tree);
-  printf("node = %d max = %d \n", root->left_val, root->max_so_far);
+  printf("node = %d max = %d \n", (root->interval).left_val, root->max_so_far);
   in_order_traversal(root->right_tree);
 }
 
 
 int main(){
-  root = insertInterval(17, 19, root);
-  root = insertInterval(5, 8, root);
-  root = insertInterval(21, 24, root);
-  root = insertInterval(4, 8, root);
-  root = insertInterval(15, 18, root);
-  root = insertInterval(7, 10, root);
-  root = insertInterval(16, 22, root);
+
+  struct interval interval1, interval2, interval3, interval4, interval5, interval6, interval7, search_interval;
+  interval1.left_val = 17;
+  interval1.right_val = 19;
+
+  interval2.left_val = 5;
+  interval2.right_val = 8;
+
+  interval3.left_val = 21;
+  interval3.right_val = 24;
+
+  interval4.left_val = 4;
+  interval4.right_val = 8;
+
+  interval5.left_val = 15;
+  interval5.right_val = 18;
+
+  interval6.left_val = 7;
+  interval6.right_val = 10;
+
+  interval7.left_val = 16;
+  interval7.right_val = 22;
+  
+  root = insertInterval(interval1, root);
+  root = insertInterval(interval2, root);
+  root = insertInterval(interval3, root);
+  root = insertInterval(interval4, root);
+  root = insertInterval(interval5, root);
+  root = insertInterval(interval6, root);
+  root = insertInterval(interval7, root);
 
   in_order_traversal(root);
+
+
+  search_interval.left_val = 21;
+  search_interval.right_val = 23;
   
-  searchIntersectingInterval(root, 21, 23);
+  searchIntersectingInterval(root, search_interval);
   
 }
--- princeton-part-1/interval_search_trees/ist.h
+++ princeton-part-1/interval_search_trees/ist.h
@@ -1,14 +1,19 @@
+struct interval{
+  int left_val, right_val;
+};
+
 struct ist_node{
-  int left_val, right_val, max_so_far;
+  struct interval interval;
+  int  max_so_far;
   struct ist_node *left_tree, *right_tree;
 };
 
-struct ist_node * insertInterval(int low, int high, struct ist_node *root);
+struct ist_node * insertInterval(struct interval interval, struct ist_node *root);
 
 void updateMaxValForNode(struct ist_node *current_node, struct ist_node *latest_node);
 
 void in_order_traversal(struct ist_node *root);
 
-void searchIntersectingInterval(struct ist_node *root, int low, int high);
+void searchIntersectingInterval(struct ist_node *root, struct interval interval);
 
-int intersects(struct ist_node *x, int low, int high);
+int intersects(struct ist_node *x, struct interval interval);
