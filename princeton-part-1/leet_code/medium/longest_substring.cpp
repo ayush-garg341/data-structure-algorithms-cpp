c8e48a3c41350911c3f27753049c29be7ca36210
Ayush Garg <ayush.garg@tripock.com>
2021-06-04 07:02:18 +0530

    sliding window on string for finding longest substring, but won't work in CP since substr will give MLE error...

--- princeton-part-1/leet_code/medium/longest_substring.cpp
+++ princeton-part-1/leet_code/medium/longest_substring.cpp
@@ -1,31 +1,23 @@
 #include<iostream>
-#include <bits/stdc++.h>
+#include<string>
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int len = 0;
-	unordered_set <char> charSet ;
+	int start = 0;
         for(unsigned int i = 0; i < s.length(); i++){
-	  int count = 0;
-	  for(unsigned int j = i; j < s.length(); j++){
-	    if (charSet.find(s[j]) == charSet.end()){
-	      charSet.insert(s[j]);
-	      count += 1;
-	    }
-	    else{
-	      break;
-	    }
+	  cout  << "substring = " << s.substr(0, i+1) << "\n";
+	  cout << "s[i] = " << s[i] << "\n";
+	  if(s.substr(0, i).rfind(s[i])!=string::npos){
+	    cout << "found = " << s[i] << "\n";
+	    start = max((unsigned)start, (unsigned)s.substr(0,i).rfind(s[i]) + 1 );
+	    cout << "start = " << start << "\n";
 	  }
-
-	  if(count > len){
-	    len = count;
-	  }
-
-	  charSet.clear();
-	  
-        }
+	  len = max((unsigned)len ,  i - start + 1);
+	  cout << "len = " << len << "\n";
+	}
         
         return len;
     }
@@ -34,6 +26,6 @@ public:
 
 int main(){
   Solution soln;
-  int len = soln.lengthOfLongestSubstring("abcabcbb");
+  int len = soln.lengthOfLongestSubstring("abba");
   cout << "len = " << len << "\n";
 }
