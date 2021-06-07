6a73535bb12d041c7ee848e36d7be562b5e81cb6
Ayush Garg <ayush.garg@tripock.com>
2021-06-07 23:30:32 +0530

    three sum in cpp in O(n^2) done with the help of two pointers....

--- princeton-part-1/leet_code/medium/three_sum.cpp
+++ princeton-part-1/leet_code/medium/three_sum.cpp
@@ -7,90 +7,58 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-      sort(nums.begin(), nums.end());
-      vector<vector<int>> pairs;
-      vector<int>pair;
-      
       vector<vector<int>> triplets;
+      sort(nums.begin(), nums.end());
+
+      set<vector<int>> set_of_vectors;
       vector<int>triplet;
       
-      for(int i=0; i < nums.size(); i++){
-	if(pair.size()==0){
-	  pair.push_back(nums[i]);
-	}
-	else{
-	  pair[0] = nums[i];
-	  pair[2] = i;
-	}
-	if(i>=1 && nums[i]==nums[i-1]){
-	  continue;
-	}
-	for(int j = i+1; j < nums.size(); j++){
+      for(int i=0; i<nums.size(); i++){
+	int k = i+1;
+	int j = nums.size()-1;
+	while(k<j){
+	  if(nums[i]+nums[k] == -nums[j]){
+	    if(triplet.size()==0){
+	      triplet.push_back(nums[i]);
+	      triplet.push_back(nums[k]);
+	      triplet.push_back(nums[j]);
+	    }
+	    else{
+	      triplet[0] =  nums[i];
+	      triplet[1] =  nums[k];
+	      triplet[2] =  nums[j];
+	    }
 
-	  if(j>=1 && nums[j]==nums[j-1]){
-	    continue;
+	    if(set_of_vectors.count(triplet)==0){
+	      set_of_vectors.insert(triplet);
+	    }
+
+	    k++;
+	    j--;
 	  }
-	  
-	  if(pair.size()==1){
-	    pair.push_back(nums[j]);
-	    pair.push_back(i);
-	    pair.push_back(j);
+
+	  else if(nums[i]+nums[k] < -nums[j]){
+	    k++;
 	  }
 	  else{
-	    pair[1] = nums[j];
-	    pair[3] = j;
+	    j--;
 	  }
-	  pairs.push_back(pair);
- 	}
+	}
       }
 
-      for(int k =0; k < pairs.size() ; k++){
-	int sum_pair = - ( pairs[k][0] + pairs[k][1]);
-	int pos = binarySearch(nums, 0, nums.size()-1, sum_pair);
-	if(pos != -1 && pairs[k][2] != pos && pairs[k][3] != pos){
-	  
-	  if(triplet.size()==0){
-	    triplet.push_back(pairs[k][0]);
-	    triplet.push_back(pairs[k][1]);
-	    triplet.push_back(nums[pos]);
-	  }
-	  else{
-	    triplet[0] = pairs[k][0];
-	    triplet[1] = pairs[k][1];
-	    triplet[2] = nums[pos];
-	  }
-	   
-	  triplets.push_back(triplet);
-	    
-	}
-      };
-
+      for (auto it = set_of_vectors.begin(); it != set_of_vectors.end(); it++) {
+        triplets.push_back(*it);
+      } 
+      
       return triplets;
     }
-
-  int binarySearch(vector<int> &arr, int l, int r, int x)
-  {
-    if (r >= l) {
-      int mid = l + (r - l) / 2;
-      
-      if (arr[mid] == x)
-	return mid;
-      
-      if (arr[mid] > x)
-	return binarySearch(arr, l, mid - 1, x);
-      
-      return binarySearch(arr, mid + 1, r, x);
-    }  
-    return -1;
-  }
 };
 
 int main(){
   Solution soln;
   vector<int> nums = {-1,0,1,2,-1,-4};
   vector <vector <int>> triplets = soln.threeSum(nums);
-
-  for(int k= 0; k < triplets.size(); k++){
-    cout << "[0] = " << triplets[k][0] << " [1] = " << triplets[k][1] << " [2] = " << triplets[k][2] << "\n";
+  for(int i=0; i<triplets.size(); i++){
+    cout << triplets[i][0] << ", " << triplets[i][1] << ", " << triplets[i][2] << "\n";
   }
 }
