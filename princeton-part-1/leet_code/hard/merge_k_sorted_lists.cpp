171c158bbea7f55d6db807555d0c2f86c7e4eee3
Ayush Garg <ayush.garg@tripock.com>
2021-06-16 01:43:41 +0530

    merge k-sorted list one by one using naive approach but improved code doen..

--- princeton-part-1/leet_code/hard/merge_k_sorted_lists.cpp
+++ princeton-part-1/leet_code/hard/merge_k_sorted_lists.cpp
@@ -8,24 +8,18 @@ public:
       ListNode *temp = NULL;
       int n = lists.size();
 
+      if(n==0){
+	return {};
+      }
+      
       if(n==1){
 	return lists[0];
       }
-      
+
+      temp = lists[0];
       
       for(int i = 1; i < n; i++){
-	if(temp==NULL){
-	  ListNode *list1 = lists[i-1];
-	  ListNode *list2 = lists[i];
-	  ListNode *merged_list_head = mergeTwoLists(list1, list2);
-	  temp = merged_list_head;
-	}
-	else{
-	  ListNode *list1 = temp;
-	  ListNode *list2 = lists[i];
-	  ListNode *merged_list_head = mergeTwoLists(list1, list2);
-	  temp = merged_list_head;
-	}
+	temp = mergeTwoLists(temp, lists[i]);
       }
 
       return temp;
@@ -34,62 +28,23 @@ public:
 
 
   ListNode* mergeTwoLists(ListNode *list1, ListNode* list2){
-    ListNode *merged_list = NULL;
-    ListNode *prev_merged = NULL;
+    ListNode *temp = new ListNode;
+    ListNode *ans = temp;
     while(list1!=NULL && list2 != NULL){
-      ListNode *temp = new ListNode;
       if(list1->val < list2->val){
-	temp -> val = list1->val;
-	temp -> next = NULL;
+	temp -> next = list1;
 	list1 = list1->next;
       }
       else{
-	temp -> val = list2->val;
-	temp -> next = NULL;
+	temp -> next = list2;
 	list2 = list2->next;	
       }
-
-      if(merged_list==NULL && prev_merged==NULL){
-	merged_list = temp;
-	prev_merged = temp;
-      }
-      else{
-	prev_merged->next = temp;
-	prev_merged = temp;
-      }
+      temp = temp->next;
     }
 
-    while(list1 != NULL){
-      ListNode *temp = new ListNode;
-      temp -> val = list1->val;
-      temp -> next = NULL;
-      list1 = list1->next;
-      if(merged_list==NULL && prev_merged==NULL){
-	merged_list = temp;
-	prev_merged = temp;
-      }
-      else{
-	prev_merged->next = temp;
-	prev_merged = temp;
-      }
-    }
+    temp -> next = (list1==NULL) ? list2: list1;
 
-    while(list2 != NULL){
-      ListNode *temp = new ListNode;
-      temp -> val = list2->val;
-      temp -> next = NULL;
-      list2 = list2->next;
-      if(merged_list==NULL && prev_merged==NULL){
-	merged_list = temp;
-	prev_merged = temp;
-      }
-      else{
-	prev_merged->next = temp;
-	prev_merged = temp;
-      }
-    }
-
-    return merged_list;
+    return ans->next;
   }
   
 };
