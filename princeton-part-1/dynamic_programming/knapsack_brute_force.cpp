5d5541d6e9e767ebf79b5841fc584a74ece978e1
Ayush Garg <ayush.garg@tripock.com>
2021-06-27 02:03:17 +0530

    knapsack brute force recursive to top down recursive with memoization...

--- princeton-part-1/dynamic_programming/knapsack_brute_force.cpp
+++ princeton-part-1/dynamic_programming/knapsack_brute_force.cpp
@@ -6,23 +6,31 @@ using namespace std;
 class Knapsack {
 public:
   int solveKnapsack(const vector<int> &profits, const vector<int> &weights, int capacity) {
-    return this->solveKnapsackRecursive(profits, weights, capacity, 0);
+    vector<vector<int>> dp(profits.size(), vector<int>(capacity + 1, -1));
+    return this->solveKnapsackRecursive(dp, profits, weights, capacity, 0);
   }
 
-  int solveKnapsackRecursive(const vector<int> &profits, const vector<int> &weights, int capacity, int currentIndex){
+  int solveKnapsackRecursive(vector<vector<int>>dp, const vector<int> &profits, const vector<int> &weights, int capacity, int currentIndex){
     if(capacity<=0 || currentIndex >= profits.size()){
       return 0;
     }
 
+    if(dp[currentIndex][capacity]!=-1){
+      return dp[currentIndex][capacity];
+    }
+    
+
     int profit1 = 0;
     
     if(weights[currentIndex] <= capacity){
-      profit1 = profits[currentIndex] + solveKnapsackRecursive(profits, weights, capacity - weights[currentIndex], currentIndex + 1);
+      profit1 = profits[currentIndex] + solveKnapsackRecursive(dp, profits, weights, capacity - weights[currentIndex], currentIndex + 1);
     }
 
-    int profit2  = solveKnapsackRecursive(profits, weights, capacity, currentIndex + 1);
+    int profit2  = solveKnapsackRecursive(dp, profits, weights, capacity, currentIndex + 1);
 
-    return max(profit1, profit2);
+    dp[currentIndex][capacity] = max(profit1, profit2);
+
+    return dp[currentIndex][capacity];
   }
   
 };
