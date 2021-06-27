329bbb181a93d6558f987e832f88a5cfcd4818e6
Ayush Garg <ayush.garg@tripock.com>
2021-06-27 13:23:38 +0530

    equal subset sum with top-down approach using memoization done...

--- princeton-part-1/dynamic_programming/equal_subset_sum.cpp
+++ princeton-part-1/dynamic_programming/equal_subset_sum.cpp
@@ -14,31 +14,37 @@ public:
     if(sum % 2 != 0){
       return false;
     }
+
+    vector<vector<int>>dp(num.size(), vector<int>(sum/2 + 1, -1));
     
-    bool can = canPartitionRecursive(num, sum/2,  0);
+    bool can = canPartitionRecursive(dp, num, sum/2,  0);
 
     return can;
   }
 
 private:
-  bool canPartitionRecursive(const vector<int> &num, int sum,  int currentIndex){
+  bool canPartitionRecursive(vector<vector<int>>dp,  const vector<int> &num, int sum,  int currentIndex){
 
     if(sum == 0){
       return true;
     }
     
     if(currentIndex >= num.size() || num.empty()){
-      return 0;
+      return false;
     }
 
-    if(num[currentIndex] <= sum){
-      if(canPartitionRecursive(num, sum - num[currentIndex], currentIndex+1)){
-	return true;
+    if(dp[currentIndex][sum]==-1){
+      if(num[currentIndex] <= sum){
+	if(canPartitionRecursive(dp, num, sum - num[currentIndex], currentIndex+1)){
+	  dp[currentIndex][sum] = 1;
+	  return true;
+	}
       }
     }
 
-    return canPartitionRecursive(num, sum, currentIndex+1);
-    
+    dp[currentIndex][sum] = canPartitionRecursive(dp, num, sum, currentIndex+1) ? 1 : 0;
+
+    return dp[currentIndex][sum] == 1 ? true : false;
     
   }
   
@@ -46,9 +52,12 @@ private:
 
 
 int main(){
-  const vector<int> nums = {1, 2, 3, 4};
-  PartitionSet ptr;
-  bool can = ptr.canPartition(nums);
-  cout << can << endl;
+  PartitionSet ps;
+  vector<int> num = {1, 2, 3, 4};
+  cout << ps.canPartition(num) << endl;
+  num = vector<int>{1, 1, 3, 4, 7};
+  cout << ps.canPartition(num) << endl;
+  num = vector<int>{2, 3, 4, 6};
+  cout << ps.canPartition(num) << endl;
   return 0;
 }
