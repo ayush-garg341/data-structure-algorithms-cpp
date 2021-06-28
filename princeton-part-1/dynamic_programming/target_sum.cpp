5bd597860164b46c4ca28839bbab7ef5f43a8183
Ayush Garg <ayush.garg@tripock.com>
2021-06-28 12:54:38 +0530

    target sum using top-down dp recursive approach done.. but with TLE..

--- princeton-part-1/dynamic_programming/target_sum.cpp
+++ princeton-part-1/dynamic_programming/target_sum.cpp
@@ -6,7 +6,11 @@ using namespace std;
 class TargetSum {
 public:
   int findTargetSubsets(const vector<int> &num, int s) {
-    vector<vector<int>>dp(num.size(), vector<int>(s+1, -1));
+    int sum = 0;
+    for(int i = 0; i < num.size(); i++){
+      sum += num[i];
+    }
+    vector<vector<int>>dp(num.size(), vector<int>(sum+1, -1));
     int count = findTargetSubsetsRecursuve(dp, num, s, 0, 0, 0);
     return count;
   }
@@ -23,15 +27,15 @@ private:
 
     int count = 0;
 
-    //if(dp[currentIndex][sum1]==-1){
-      count += findTargetSubsetsRecursuve(dp, num, sum,  sum1-num[currentIndex], sum2,  currentIndex+1);
+    if(dp[currentIndex][sum1]==-1){
+      count += findTargetSubsetsRecursuve(dp, num, sum,  sum1 + num[currentIndex], sum2,  currentIndex+1);
       
-      count += findTargetSubsetsRecursuve(dp, num, sum, sum1, sum2-num[currentIndex], currentIndex+1);
+      count += findTargetSubsetsRecursuve(dp, num, sum, sum1, sum2 + num[currentIndex], currentIndex+1);
 
-      //dp[currentIndex][sum1] = count;
-      //}
-      //return dp[currentIndex][sum1];
-    return count;
+      dp[currentIndex][sum1] = count;
+    }
+    return dp[currentIndex][sum1];
+    //return count;
   }
   
 };
