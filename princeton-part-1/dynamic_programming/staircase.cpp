15f3e98188088d7302b1edcb5f1c3f074c7f1908
Ayush Garg <ayush.garg@tripock.com>
2021-07-02 06:27:32 +0530

    staircase using top-bottom dp approach done...

--- princeton-part-1/dynamic_programming/staircase.cpp
+++ princeton-part-1/dynamic_programming/staircase.cpp
@@ -1,23 +1,32 @@
 using namespace std;
 
 #include <iostream>
+#include<vector>
 
 class Staircase {
 
 public:
   int CountWays(int n) {
-    if(n < 0){
-      return 0;
-    }
-    if(n<=1){
-      return 1;
-    }
-    if(n==2){
-      return 2;
-    }
-    int result = CountWays(n-1) + CountWays(n-2) + CountWays(n-3);
+    vector<int>memoize(n+1, 0);
+    memoize[0] = 1;
+    memoize[1] = 1;
+    memoize[2] = 2;
+
+    int result  = CountWaysRecursive(memoize, n);
     return result;
   }
+
+private:
+  int CountWaysRecursive(vector<int>memoize, int n){
+    if(memoize[n]){
+      return memoize[n];
+    }
+
+    memoize[n] = CountWaysRecursive(memoize, n-1) + CountWaysRecursive(memoize, n-2) + CountWaysRecursive(memoize, n-3);
+
+    return memoize[n];
+  }
+  
 };
 
 int main(int argc, char *argv[]) {
