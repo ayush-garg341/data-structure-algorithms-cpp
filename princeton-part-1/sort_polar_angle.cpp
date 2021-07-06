a7a26cfe7bfaea32b0b7fae7f50bd2b447580fbc
Ayush Garg <ayush.garg@tripock.com>
2021-07-06 11:12:52 +0530

    polar angle imprved but not completely correct...

--- princeton-part-1/sort_polar_angle.cpp
+++ princeton-part-1/sort_polar_angle.cpp
@@ -7,18 +7,46 @@ using namespace std;
 
 pair<int, int> refPoint = make_pair(0, 0);
 
+
+int quadrant(lpr x){
+  if(x.first >= 0 && x.second >= 0){
+    return 1;
+  }
+  if(x.first <=0 && x.second >= 0){
+    return 2;
+  }
+  if(x.first <=0 && x.second <= 0){
+    return 3;
+  }
+
+  if(x.first >= 0 && x.second <= 0){
+    return 4;
+  }
+  
+}
+
 bool compareFn(lpr a, lpr b){
-  int a_x = a.first - refPoint.first;
-  int a_y = a.second - refPoint.second;
+  int quada = quadrant(a);
+  int quadb = quadrant(b);
 
-  int b_x = b.first - refPoint.first;
-  int b_y = b.second - refPoint.second;
+  if(quada > quadb){
+    return false;
+  }
 
-  cout << "( " << a_x << " " << a_y << " )" << " ( " << b_x << " " << b_y << " )" << endl;
+  // compute the cross product of vectors (refPoint -> a) first (refPoint -> b)
+  int det = (a.first - refPoint.first) * (b.second - refPoint.second) - (b.first - refPoint.first) * (a.second - refPoint.second);
+  if (det > 0)
+    return true;
+  if (det < 0)
+    return false;
 
-  cout << b_x*a_y - b_y*a_x << endl;
 
-  return b_x*a_y - b_y*a_x > 0 ? 1 : 0;
+  // points a and b are on the same line from the refPoint
+  // check which point is closer to the refPoint
+  int d1 = (a.first - refPoint.first) * (a.first - refPoint.first) + (a.second - refPoint.second) * (a.second - refPoint.second);
+  int d2 = (b.first - refPoint.first) * (b.first - refPoint.first) + (b.second - refPoint.second) * (b.second - refPoint.second);
+  return d1 < d2;
+  
 }
 
 
@@ -28,6 +56,9 @@ int main(){
   points.push_back(make_pair(1, 2));
   points.push_back(make_pair(3, -1));
   points.push_back(make_pair(-2, -2));
+  points.push_back(make_pair(2, 2));
+  points.push_back(make_pair(1, 3));
+  points.push_back(make_pair(3, 3));
   
   sort(points.begin(), points.end(), compareFn);
 
@@ -36,3 +67,6 @@ int main(){
   }
   return 0;
 }
+
+
+// This “comparator” function returns a value; convertible to bool, which basically tells us whether the passed “first” argument should be placed before the passed “second” argument or not. 
