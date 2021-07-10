082bcbe3811954b579128d74cbcfc712a1bff281
Ayush Garg <ayush.garg@tripock.com>
2021-07-10 10:08:11 +0530

    level order traversal with elements at a level grouped in vector done....

--- princeton-part-1/leet_code/medium/level_order_traversal.cpp
+++ princeton-part-1/leet_code/medium/level_order_traversal.cpp
@@ -18,24 +18,36 @@ struct Node{
 
 class Solution {
 public:
-    vector<int> levelOrder(Node* root) {
+  vector<vector<int>> levelOrder(Node* root) {
 
-      vector<int>level_order;
-      
-      queue<Node *>q;
+    vector<vector<int>>level_order;
+    
+    if(root==nullptr){
+      return level_order;
+    }
+    
+    queue<Node *>q;
       q.push(root);
       while(!q.empty()){
-	struct Node* root = q.front();
-	level_order.push_back(root->data);
-	q.pop();
-	if(root->left!=nullptr){
-	  q.push(root->left);
+	vector<int> temp;
+	int size = q.size();
+	while(size!=0){
+	  struct Node* root = q.front();
+	  temp.push_back(root->data);
+	  q.pop();
+	  size--;
+	  if(root->left!=nullptr){
+	    q.push(root->left);
+	  }
+	  if(root->right!=nullptr){
+	    q.push(root->right);
+	  }
 	}
-	if(root->right!=nullptr){
-	  q.push(root->right);
+	if(!temp.empty()){
+	  level_order.push_back(temp);
 	}
       }
-
+      
       return level_order;
       
     }
@@ -43,20 +55,24 @@ public:
 
 int main(int argc, char* argv[]){
 
-  struct Node* root = new Node(10);
-  root->left        = new Node(6);
-  root->right       = new Node(14);
-  root->left->left  = new Node(5);
-  root->left->right = new Node(8);
-  root->right->left  = new Node(11);
-  root->right->right = new Node(18);
+  struct Node* root = new Node(3);
+  root->left        = new Node(9);
+  root->right       = new Node(20);
+  root->right->left  = new Node(15);
+  root->right->right = new Node(7);
   
   Solution soln;
 
-  vector<int>nums  = soln.levelOrder(root);
+  vector<vector<int>>nums  = soln.levelOrder(root);
 
   for(int i = 0; i < nums.size(); i++){
-    cout << nums[i] << " -> ";
+    cout << "[ " ;
+    for(int j = 0; j < nums[i].size(); j++){
+      cout <<  nums[i][j] << " ";
+    }
+    cout << "]";
+
+    cout << endl;
   }
 
   cout << endl;
