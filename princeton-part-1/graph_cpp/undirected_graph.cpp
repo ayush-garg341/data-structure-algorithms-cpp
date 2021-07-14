32739b72ef5f57f1f148072f1b169eab4ef19fa3
Ayush Garg <ayush.garg@tripock.com>
2021-07-14 21:50:39 +0530

    underiected graph dfs and bfs traversal cpp done...

--- princeton-part-1/graph_cpp/undirected_graph.cpp
+++ princeton-part-1/graph_cpp/undirected_graph.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<stack>
+#include<queue>
 using namespace std;
 
 void addEdge(vector<int>adj[], int from, int to){
@@ -45,29 +46,66 @@ void printVertexSource(int edgeTo[], bool marked[], int V){
   }
 }
 
-void pathToVertex(int edgeTo[], int vertex){
-  if(edgeTo[vertex]!=vertex){
-    pathToVertex(edgeTo, edgeTo[vertex]);
-    cout << edgeTo[vertex] << " -> ";
+stack<int> pathToVertex(int edgeTo[], int vertex){
+  stack<int>path;
+  int dest = vertex;
+  path.push(dest);
+  while(edgeTo[dest]!=dest){
+    path.push(edgeTo[dest]);
+    dest = edgeTo[dest];
   }
-  return;
+
+  return path;
+};
+
+
+void bfs(vector<int>adj[],  int edgeTo[], bool marked[], int source){
+  queue<int>vertices;
+  vertices.push(source);
+  edgeTo[source] = source;
+  marked[source] = true;
+  while(!vertices.empty()){
+    int current = vertices.front();
+    vertices.pop();
+    for(auto x: adj[current]){
+      if(!marked[x]){
+	marked[x] = true;
+	edgeTo[x] = current;
+	vertices.push(x);
+      }
+    }
+    cout << endl;
+  }
 }
 
 
+int distanceFromSource(int edgeTo[], int dest){
+  int count = 0;
+  while(edgeTo[dest]!=dest){
+    count++;
+    dest = edgeTo[dest];
+  }
+
+  return count;
+}
+
+
+
 int main(int argc, char* argv[]){
-  int V = 5;
+  int V = 6;
 
   // can be written like this....
   // vector<int>* adj = new vector<int>[V];
   
   vector<int>adj[V];
+  addEdge(adj, 0, 5);
   addEdge(adj, 0, 1);
-  addEdge(adj, 0, 4);
-  addEdge(adj, 1, 2);
-  addEdge(adj, 1, 3);
-  addEdge(adj, 1, 4);
+  addEdge(adj, 0, 2);
   addEdge(adj, 2, 3);
+  addEdge(adj, 2, 4);
+  addEdge(adj, 1, 2);
   addEdge(adj, 3, 4);
+  addEdge(adj, 3, 5);
   printGraph(adj, V);
 
   cout << endl;
@@ -83,8 +121,30 @@ int main(int argc, char* argv[]){
   printVertexSource(edgeTo, marked,  V);
 
   int dest = 4;
-  pathToVertex(edgeTo, dest);
-  cout << dest << endl;
+  stack<int>path = pathToVertex(edgeTo, dest);
+  while(!path.empty()){
+    cout << path.top() << " -> ";
+    path.pop();
+  }
+
+  cout << endl;
+
+
+  /* ----------------------- BFS processing --------------------- */
+
+  // marked[V] = {false};
+
+  for(int i = 0; i < V; i++){
+    marked[i] = false;
+  }
+  
+  bfs(adj, edgeTo, marked, source);
+
+  printVertexSource(edgeTo, marked,  V);
+
+  dest = 4;
+  int distance = distanceFromSource(edgeTo, dest);
+  cout << "distance to " << dest << " from source " << source <<   " = " << distance << endl;
   
   return 0;
 }
