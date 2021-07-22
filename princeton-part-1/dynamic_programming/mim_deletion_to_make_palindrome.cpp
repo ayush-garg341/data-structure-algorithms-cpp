6823d79d83a29c8780470d78e1a6fbae5b759d27
Ayush Garg <ayush.garg@tripock.com>
2021-07-22 22:59:28 +0530

    mim deleeions to make string palindrome done...

---
+++ princeton-part-1/dynamic_programming/mim_deletion_to_make_palindrome.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+class LPS {
+public:
+  int findLPSLength(const string &st) {
+    vector<vector<int>>dp(st.length(), vector<int>(st.length(), 0));
+    for(int i = 0; i < st.length(); i++){
+      dp[i][i] = 1;
+    }
+
+    for(int l = 2; l <= st.length(); l++){
+      for(int i = 0; i < st.length()-l+1; i++){
+	int j = i + l -1;
+	if(l==2 && st[i] == st[j]){
+	  dp[i][j] = 2;
+	}
+	else if(st[i] == st[j]){
+	  dp[i][j] = 2 + dp[i+1][j-1];
+	}
+	else{
+	  dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+	}
+      }
+    }
+
+    return st.length() - dp[0][st.length()-1];
+    
+  }
+  
+};
+
+
+int main(int argc, char *argv[]){
+  LPS *lps = new LPS();
+  cout << lps->findLPSLength("abdbca") << endl;
+  cout << lps->findLPSLength("cddpd") << endl;
+  cout << lps->findLPSLength("pqr") << endl;
+  cout << lps->findLPSLength("geeksforgeeks") << endl;
+  delete lps;
+  
+  return 0;
+}
