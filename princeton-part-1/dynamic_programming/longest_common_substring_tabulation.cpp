8a032acf1177fff7407118a4c4042d074fbd85de
Ayush Garg <ayush.garg@tripock.com>
2021-07-23 13:17:16 +0530

    some fixes done in longest common substring using tabulation part....

--- princeton-part-1/dynamic_programming/longest_common_substring_tabulation.cpp
+++ princeton-part-1/dynamic_programming/longest_common_substring_tabulation.cpp
@@ -7,9 +7,9 @@ public:
   int findLCSLength(const string &s1, const string &s2) {
     vector<vector<int>>dp(s1.length()+1, vector<int>(s2.length()+1, 0));
     int maxLength = 0;
-    for(int i = 1; i < s1.length(); i++){
-      for(int j = 1; j < s2.length(); j++){
-	if(s1[i] == s2[j]){
+    for(int i = 1; i <= s1.length(); i++){
+      for(int j = 1; j <= s2.length(); j++){
+	if(s1[i-1] == s2[j-1]){
 	  dp[i][j] = 1 + dp[i-1][j-1];
 	}
 	else{
