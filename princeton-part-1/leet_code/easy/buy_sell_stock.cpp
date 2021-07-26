20242a34b48f63270d06272e74ea51ed696ed445
Ayush Garg <ayush.garg@tripock.com>
2021-07-26 08:13:46 +0530

    memory optimized for buy and sell stocks....

--- princeton-part-1/leet_code/easy/buy_sell_stock.cpp
+++ princeton-part-1/leet_code/easy/buy_sell_stock.cpp
@@ -6,15 +6,17 @@ using namespace std;
 class Solution {
 public:
   int maxProfit(vector<int>& prices) {
-    vector<int>dp(prices.size()+1);
+    //vector<int>dp(prices.size()+1);
     int min_so_far = prices[0];
-    dp[0] = 0;
+    //dp[0] = 0;
+    int max_profit = 0;
     for(int i = 1; i <= prices.size(); i++){
-      dp[i] = max(dp[i-1], prices[i-1]-min_so_far);
+      max_profit = max(max_profit, prices[i-1]-min_so_far);
       min_so_far = min(min_so_far, prices[i-1]);
     }
 
-    return dp[prices.size()];
+    //return dp[prices.size()];
+    return max_profit;
   }
 };
 
