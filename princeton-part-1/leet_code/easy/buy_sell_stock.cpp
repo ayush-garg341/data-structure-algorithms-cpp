64c6fcc0f3b0c4fce809e4a281ee0a89e68bf8de
Ayush Garg <ayush.garg@tripock.com>
2021-07-26 08:28:23 +0530

    second aproach to find max profit by making a price changes array and then calculating max sub-array sum done....

--- princeton-part-1/leet_code/easy/buy_sell_stock.cpp
+++ princeton-part-1/leet_code/easy/buy_sell_stock.cpp
@@ -6,17 +6,43 @@ using namespace std;
 class Solution {
 public:
   int maxProfit(vector<int>& prices) {
-    //vector<int>dp(prices.size()+1);
-    int min_so_far = prices[0];
-    //dp[0] = 0;
-    int max_profit = 0;
-    for(int i = 1; i <= prices.size(); i++){
-      max_profit = max(max_profit, prices[i-1]-min_so_far);
-      min_so_far = min(min_so_far, prices[i-1]);
+    // //vector<int>dp(prices.size()+1);
+    // int min_so_far = prices[0];
+    // //dp[0] = 0;
+    // int max_profit = 0;
+    // for(int i = 1; i <= prices.size(); i++){
+    //   max_profit = max(max_profit, prices[i-1]-min_so_far);
+    //   min_so_far = min(min_so_far, prices[i-1]);
+    // }
+
+    // //return dp[prices.size()];
+    // return max_profit;
+
+    /* second approach using changes in prices and computing max sub-array sum */
+
+    vector<int>change_price(prices.size()-1);
+    for(int i = 1; i < prices.size(); i++){
+      change_price.push_back(prices[i] - prices[i-1]);
     }
 
-    //return dp[prices.size()];
-    return max_profit;
+    /* use kadane's algorithm to find max. sub-array sum to find the max. profit */
+    int max_sum = 0;
+    int find_negative = 0;
+    for(int i = 0; i < change_price.size(); i++){
+      if(change_price[i] <= find_negative + change_price[i]){
+	find_negative += change_price[i];
+      }
+      else{
+	find_negative = change_price[i];
+      }
+
+      if(find_negative > max_sum){
+	max_sum = find_negative;
+      }
+    }
+
+    return max_sum;
+    
   }
 };
 
